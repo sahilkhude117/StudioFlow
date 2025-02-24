90da14e2f5b1533d2fad9f1d27cc695459f7a5f1
sahilkhude117 <sahilkhude11@gmail.com>
2025-02-24 19:24:37 +0530

    flows

---
+++ packages/web/src/pages/Flows/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Container from "components/Container";
+
+export default function Flows() {
+    return <>
+        <Box sx={{ py: 3}}>
+            <Container>
+                Flows
+            </Container>
+        </Box>
+    </>
+}
