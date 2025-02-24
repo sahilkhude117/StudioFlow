b5c94d333940f469721f21036d5d24e3aa8ff93d
sahilkhude117 <sahilkhude11@gmail.com>
2025-02-24 19:22:03 +0530

    Notifications

---
+++ packages/web/src/hooks/useStudioFlowNotifications.js
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import api from 'helpers/api';
+
+export default function useStudioFlowNotifications() {
+    const query = useQuery({
+        queryKey: ['automatisch', 'notifications'],
+        queryFn: async ({ signal }) => {
+            const { data } = await api.get(`/v1/automatisch/notifications`, {
+                signal
+            });
+
+            return data;
+        },
+    });
+
+    return query;
+}
