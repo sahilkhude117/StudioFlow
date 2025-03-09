f2e429ee0563bbd9b721b3683050e3056040a378
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-09 21:37:46 +0530

    service: trigger

---
+++ apps/api/src/services/trigger.ts
@@ -0,0 +1,39 @@
+import Step from "../models/step";
+import Flow from "../models/flow";
+import Execution from "../models/execution";
+import globalVariable from "../helpers/global-variable";
+
+export const processTrigger = async (options:any) => {
+    const { flowId, stepId, triggerItem, error, testRun } = options;
+
+    const step = await Step.query().findById(stepId).throwIfNotFound();
+
+    const $ = await globalVariable({
+        flow: await Flow.query().findById(flowId).throwIfNotFound(),
+        //@ts-ignore
+        app: await step.getApp(),
+        step: step,
+        connection: await step.$relatedQuery('connection'),
+    });
+
+    // check if we already process this trigger data item or not!
+
+    const execution = await Execution.query().insert({
+        flowId: $.flow.id,
+        testRun,
+        internalId: triggerItem?.meta.internalId,
+    });
+
+    const executionStep = await execution
+        .$relatedQuery('executionSteps')
+        .insertAndFetch({
+            stepId: $.step.id,
+            status: error ? 'failure' : 'success',
+            dataIn: $.step.parameters,
+            dataOut: !error ? triggerItem?.raw : null,
+            errorDetails: error,
+        });
+    
+    //@ts-ignore
+    return { flowId, stepId, executionId: execution.id, executionStep}
+}
