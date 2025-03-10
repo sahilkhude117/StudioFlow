e2946caa0451f4e09f12cfa52d4f75674c26d57a
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-10 22:06:30 +0530

    model: usage-data

---
+++ apps/api/src/models/usage-data.ts
@@ -0,0 +1,52 @@
+import Base from "./base";
+import { Raw } from "objection";
+import User from "./user";
+import Subscription from "./subscription.ee";
+
+class UsageData extends Base {
+    static tableName = 'usage_data';
+
+    static jsonSchema = {
+        type: 'object',
+        required: ['userId', 'consumedTaskCount', 'nextResetAt'],
+
+        properties: {
+        id: { type: 'string', format: 'uuid' },
+        userId: { type: 'string', format: 'uuid' },
+        subscriptionId: { type: 'string', format: 'uuid' },
+        consumedTaskCount: { type: 'integer' },
+        nextResetAt: { type: 'string' },
+        deletedAt: { type: 'string' },
+        createdAt: { type: 'string' },
+        updatedAt: { type: 'string' },
+    },
+  };
+
+  static relationMappings = () => ({
+    user: {
+      relation: Base.BelongsToOneRelation,
+      modelClass: User,
+      join: {
+        from: 'usage_data.user_id',
+        to: 'users.id',
+      },
+    },
+    subscription: {
+      relation: Base.BelongsToOneRelation,
+      modelClass: Subscription,
+      join: {
+        from: 'usage_data.subscription_id',
+        to: 'subscriptions.id',
+      },
+    },
+  });
+
+  async increaseConsumedTaskCountByOne() {
+    return await this.$query().patch({
+        //@ts-ignore
+      consumedTaskCount: raw('consumed_task_count + 1'),
+    });
+  }
+}
+
+export default UsageData;
