1e1609ef136a013330c5590fc6286c871d1c842b
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-10 22:07:50 +0530

    helper: billing

---
+++ apps/api/src/helpers/billing/index.ee.ts
@@ -0,0 +1,18 @@
+import appConfig from "../../config/app";
+import paddleClient from "./paddle.ee";
+import paddlePlans from './plans.ee';
+import webhooks from "./webhooks.ee";
+
+const paddleInfo = {
+    sandbox: appConfig.isProd ? false : true,
+    vendorId: appConfig.paddleVendorId,
+};
+
+const billing = {
+    paddleClient,
+    paddlePlans,
+    paddleInfo,
+    webhooks,
+  };
+  
+  export default billing;
