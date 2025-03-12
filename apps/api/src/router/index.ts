6f3c317e781f22bc9a247b34dcc5096971a19fb6
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-12 22:44:43 +0530

    route: paddle

--- apps/api/src/router/index.ts
+++ apps/api/src/router/index.ts
@@ -2,11 +2,13 @@
 import { Router } from "express";
 import { healthRouter } from "./healthcheck";
 import webhooksRouter from "./webhooks";
+import paddleRouter from './paddle.ee.js';
 
 const router = Router();
 
 router.use("/health", healthRouter);
 router.use('/webhooks', webhooksRouter);
+router.use('/paddle', paddleRouter);
 
 export const mainRouter:Router = router;
 
