125838535868b8edd7b1f45a163cf6895642450c
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-12 22:40:24 +0530

    route: webhooksRouter

--- apps/api/src/router/index.ts
+++ apps/api/src/router/index.ts
@@ -1,10 +1,12 @@
 
 import { Router } from "express";
 import { healthRouter } from "./healthcheck";
+import webhooksRouter from "./webhooks";
 
 const router = Router();
 
 router.use("/health", healthRouter);
+router.use('/webhooks', webhooksRouter);
 
 export const mainRouter:Router = router;
 
