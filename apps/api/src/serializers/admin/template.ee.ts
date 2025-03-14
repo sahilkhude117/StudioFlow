0eede9b4e7fab67784d09a0d68d7ad95c5408bab
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-14 23:25:17 +0530

    serializer: admin

---
+++ apps/api/src/serializers/admin/template.ee.ts
@@ -0,0 +1,13 @@
+//@ts-ignore
+const adminTemplateSerializer = (template) => {
+    return {
+      id: template.id,
+      name: template.name,
+      flowData: template.flowData,
+      createdAt: template.createdAt.getTime(),
+      updatedAt: template.updatedAt.getTime(),
+    };
+};
+  
+export default adminTemplateSerializer;
+  
