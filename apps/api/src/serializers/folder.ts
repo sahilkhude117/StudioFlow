bfd6668aae070de3ae73f8368c5dcea98e6a448c
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-14 23:27:47 +0530

    serializer: folder

---
+++ apps/api/src/serializers/folder.ts
@@ -0,0 +1,12 @@
+//@ts-ignore
+const folderSerilializer = (folder) => {
+    return {
+      id: folder.id,
+      name: folder.name,
+      createdAt: folder.createdAt.getTime(),
+      updatedAt: folder.updatedAt.getTime(),
+    };
+};
+  
+export default folderSerilializer;
+  
