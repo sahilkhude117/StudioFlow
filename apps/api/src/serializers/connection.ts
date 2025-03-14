5f8ff6b1bec7fc11f2a73d87a3ee797c5d674441
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-14 23:26:59 +0530

    serializer: connection

---
+++ apps/api/src/serializers/connection.ts
@@ -0,0 +1,17 @@
+//@ts-ignore
+const connectionSerializer = (connection) => {
+    return {
+      id: connection.id,
+      key: connection.key,
+      oauthClientId: connection.oauthClientId,
+      formattedData: {
+        screenName: connection.formattedData.screenName,
+      },
+      verified: connection.verified,
+      createdAt: connection.createdAt.getTime(),
+      updatedAt: connection.updatedAt.getTime(),
+    };
+};
+  
+export default connectionSerializer;
+  
