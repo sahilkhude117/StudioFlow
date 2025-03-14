acb8481d39c4826c70d04fd96d5d18bdd4e05e6c
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-14 23:26:12 +0530

    serializer: app-config

---
+++ apps/api/src/serializers/app-config.ts
@@ -0,0 +1,13 @@
+//@ts-ignore
+const appConfigSerializer = (appConfig) => {
+    return {
+      key: appConfig.key,
+      useOnlyPredefinedAuthClients: appConfig.useOnlyPredefinedAuthClients,
+      disabled: appConfig.disabled,
+      createdAt: appConfig.createdAt.getTime(),
+      updatedAt: appConfig.updatedAt.getTime(),
+    };
+};
+  
+export default appConfigSerializer;
+  
