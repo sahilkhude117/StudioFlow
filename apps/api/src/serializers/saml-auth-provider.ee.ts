0bf52045d9b6e28e502f6c6cf4edbaef0fe22576
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-14 23:29:11 +0530

    serializer: auth provider

---
+++ apps/api/src/serializers/saml-auth-provider.ee.ts
@@ -0,0 +1,11 @@
+//@ts-ignore
+const samlAuthProviderSerializer = (samlAuthProvider) => {
+    return {
+        id: samlAuthProvider.id,
+        name: samlAuthProvider.name,
+        loginUrl: samlAuthProvider.loginUrl,
+        issuer: samlAuthProvider.issuer,
+    }
+}
+
+export default samlAuthProviderSerializer;
