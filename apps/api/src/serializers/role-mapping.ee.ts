dae7247699754f5ced9bb146a3743f334dd21c1b
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-14 23:28:56 +0530

    serializer: role-mapping

---
+++ apps/api/src/serializers/role-mapping.ee.ts
@@ -0,0 +1,12 @@
+
+//@ts-ignore
+const roleMappingSerializer = (roleMapping) => {
+    return {
+        id: roleMapping.id,
+        samlAuthProviderId: roleMapping.samlAuthProviderId,
+        roleId: roleMapping.roleId,
+        remoteRoleName: roleMapping.remoteRoleName,
+    }
+};
+
+export default roleMappingSerializer;
