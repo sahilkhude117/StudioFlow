9f330f961085ae5deecc3819a991f5326f19db59
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-22 21:06:13 +0530

    feat: nav to flows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
+'use client'
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Button</Button>
+    <div className="bg-background flex h-screen w-screen items-center justify-center">
+      <Button
+        onClick={() => router.push('/flows')}
+      >Go To Flows</Button>
     </div>
   );
 }
