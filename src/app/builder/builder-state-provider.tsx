b85378b2521b1fb5b4b48810930f0fa6239e0d6a
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-22 12:50:23 +0530

    feat: added builder state provider

--- src/app/builder/builder-state-provider.tsx
+++ src/app/builder/builder-state-provider.tsx
@@ -1,4 +1,8 @@
-import { BuilderInitialState } from "./builder-hooks";
+'use client'
+import { useRef } from "react";
+import { BuilderInitialState, BuilderStateContext, BuilderStore, createBuilderStore } from "./builder-hooks";
+import { useSearchParams } from "next/navigation";
+import { NEW_FLOW_QUERY_PARAM } from "@/lib/utils";
 
 type BuilderStateProviderProps = React.PropsWithChildren<BuilderInitialState>;
 
@@ -8,5 +12,25 @@ export function BuilderStateProvider({
     sampleDataInput,
     ...props
 }: BuilderStateProviderProps) {
-    
+    const storeRef = useRef<BuilderStore | null>(null);
+    const checkAccess = true;
+    const readonly = false;
+    const searchParams = useSearchParams();
+    if (!storeRef.current) {
+        storeRef.current = createBuilderStore(
+            {
+                ...props,
+                readonly,
+                sampleData,
+                sampleDataInput
+            },
+            searchParams.get(NEW_FLOW_QUERY_PARAM) === "true",
+        );
+    }
+
+    return (
+        <BuilderStateContext.Provider value={storeRef.current}>
+            {children}
+        </BuilderStateContext.Provider>
+    )
 }
