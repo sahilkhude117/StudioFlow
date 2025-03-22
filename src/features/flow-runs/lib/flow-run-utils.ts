0236a413c068d4fe0b9e143e2c8c41b82a2d6dab
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-22 21:02:16 +0530

    util: findLoopsState

--- src/features/flow-runs/lib/flow-run-utils.ts
+++ src/features/flow-runs/lib/flow-run-utils.ts
@@ -1,7 +1,8 @@
-import { ActionType, isNil, LoopStepOutput, StepOutput, StepOutputStatus } from "../../../../shared/src";
+import { ActionType, FlowRun, flowStructureUtil, FlowVersion, isNil, LoopStepOutput, StepOutput, StepOutputStatus } from "../../../../shared/src";
 
 export const flowRunUtils = {
     findFailedStepInOutput,
+    findLoopsState,
 }
 
 function findFailedStepInOutput(
@@ -27,4 +28,26 @@ function findFailedStepInLoop(loopStepResult: LoopStepOutput): string | null {
         if (failedStep) return failedStep;
     }
     return null;
+}
+
+function findLoopsState(
+    flowVersion: FlowVersion,
+    run: FlowRun,
+    currentLoopsState: Record<string, number>,
+) {
+    const loops = flowStructureUtil
+        .getAllSteps(flowVersion.trigger)
+        .filter((s) => s.type === ActionType.LOOP_ON_ITEMS);
+    const failedStep = run.steps ? findFailedStepInOutput(run.steps) : null;
+
+    return loops.reduce(
+        (res, step) => ({
+            ...res,
+            [step.name]:
+                failedStep && flowStructureUtil.isChildOf(step, failedStep)
+                    ? Number.MAX_SAFE_INTEGER
+                    : currentLoopsState[step.name] ?? 0,
+        }),
+        currentLoopsState,
+    );
 }
