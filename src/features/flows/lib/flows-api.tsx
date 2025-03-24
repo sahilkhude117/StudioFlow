f721789d658d76aa21c4dc3c355d41d99372c068
sahilkhude117 <sahilkhude11@gmail.com>
2025-03-24 21:26:26 +0530

    feature: flows

--- src/features/flows/lib/flows-api.tsx
+++ src/features/flows/lib/flows-api.tsx
@@ -22,6 +22,42 @@ export const flowsApi = {
         request?:GetFlowQueryParamsRequest,
     ): Promise<PopulatedFlow> {
         return api.get<PopulatedFlow>(`/v1/flows/${flowId}`, request)
+    },
+    update(
+        flowId: string,
+        request: FlowOperationRequest,
+        showErrorToast = false,
+    ) {
+        return api
+            .post<PopulatedFlow>(`/v1/flows/${flowId}`, request)
+            .catch((error) => {
+                if (showErrorToast) {
+                  const errorCode: ErrorCode | undefined = (
+                    error.response?.data as { code: ErrorCode }
+                  )?.code;
+                  if (errorCode === ErrorCode.FLOW_IN_USE) {
+                    toast({
+                      title: ('Flow Is In Use'),
+                      description: (
+                        'Flow is being used by another user, please try again later.'
+                      ),
+                      duration: Infinity,
+                      action: (
+                        <Button
+                          onClick={() => window.location.reload()}
+                          size={'sm'}
+                          variant={'outline'}
+                        >
+                          {('Refresh')}
+                        </Button>
+                      ),
+                    });
+                  } else {
+                    toast(UNSAVED_CHANGES_TOAST);
+                  }
+                }
+                throw error;
+            });
     }
 }
 
